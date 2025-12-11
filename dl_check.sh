23c917b5215107b0682c7cc0493808c74f7d0fad
The-BB <tun.chen.bo@gmail.com>
2025-12-11 09:24:38 +0300

    dl_check.sh: merge, 2025.12.11

--- dl_check.sh
+++ dl_check.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-[ -x "./staging_dir/host/bin/zstd" ] || \
-{ printf '\033[31m%s\033[m' "WARNING: some tools are missing, please run \"make tools\" before re-running this script." && \
-  exit 1 ; }
-
 if [ -e .config ]; then
     read -p 'Please note: .config will be rewriten during downloading! Press [Ctrl]+[C] to abort or [Enter] to continue... ' key
 fi
